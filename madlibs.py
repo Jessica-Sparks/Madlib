dacfbf1e0f63f653afef05da4c4bbff13304cf48
Jessica-Sparks <115658126+Jessica-Sparks@users.noreply.github.com>
2023-06-07 10:32:28 -0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+#prompt a user to enter a name:
+name = input('What is your name? ')
+#prompt user fpr an adjective
+adjective = input('Enter an adjective: ')
+#prompt user for a Plural noun
+noun1 = input('Enter a Plural noun: ')
+#prompt user for a noun, either person or a thing
+noun2 = input('Enter any noun: ')
+
+#print Ryme
+print("Hello, " + name)
+print("You are " + adjective)
+print(noun1 + " ")
+print("I can't start my day without", noun2)
